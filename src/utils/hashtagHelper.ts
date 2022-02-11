76ecb56d954381a823cefe477fe80e3c09276899
Cameron McCormack <cameron.mccormack@softwire.com>
2022-02-11 20:34:53 +0000

    make channel names work like hashtags

--- src/utils/hashtagHelper.ts
+++ src/utils/hashtagHelper.ts
@@ -1,2 +1,17 @@
-export const getHashtags = (text: string): string[] =>
-    text.match(/(?<=\B#)\w*[A-Za-z0-9_-]\w*\b(?!#)/g) ?? [];
+export function getHashtags(text: string): string[] {
+    // priority order for hashtags is:
+    // 1. non-channel hashtags (in order of appearance)
+    // 2. channel names (in order of appearance)
+    const nonChannelHastags = text.match(/(?<=\B#)\w*[A-Za-z0-9_-]\w*\b(?!#)/g) ?? [];
+    const channelHashtags = (text.match(/(?<=\B<#[CDG])[^<>@]*(?=>)/g) ?? [])
+        .map(channel => {
+            const splitTag = channel.split('|');
+            if (splitTag.length < 2) {
+                return null;
+            } else {
+                return splitTag[1];
+            }
+        })
+        .filter(Boolean);
+    return nonChannelHastags.concat(channelHashtags);
+}
